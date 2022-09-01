44e5c1723aa2089ab29c1bb83b8c6e18f1cb921f
prince <prnzthmz@gmail.com>
2022-09-01 10:50:00 +0530

    google verification

--- helpers/userhelpers.js
+++ helpers/userhelpers.js
@@ -7,7 +7,7 @@ const Razorpay = require('razorpay');
 const path = require('path')
 require('dotenv').config({
     path: path.resolve(__dirname, '../.env')
-  })
+})
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
     key_secret: process.env.RAZORPAY_SCRETKEY,
